342e895f00fd5bc739e396c7e6cb0006118b8390
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2025-09-28 21:36:57 +0900

    ヘッダーなどの説明を仮追加

--- src/Assets/Scripts/Masa script/Enemy/EnemyActBase.cs
+++ src/Assets/Scripts/Masa script/Enemy/EnemyActBase.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class EnemyActBase : MonoBehaviour
 {
+    /*
+    [Header("基底クラス")]
+    [SerializeField] bool a;
+    [Header("ここから派生クラス"),Space]
+    */
+
     public virtual void Act_Start()
     {
         return;
