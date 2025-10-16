8d660a41bfe39e32ffd0a4f54fd814669d5ef72e
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2025-10-17 00:56:50 +0900

    ゲームクリア処理

--- src/Assets/Scripts/amedama script/BossTeleport.cs
+++ src/Assets/Scripts/amedama script/BossTeleport.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [Serializable]
 class TeleportClass
@@ -15,6 +16,7 @@ public class BossTeleport : MonoBehaviour
 {
 
     [SerializeField] TeleportClass[] Teleport;
+    [SerializeField] UnityEvent ClearEvent;
     int BombHitCount = 0;
     int BossState = 0;
 
@@ -35,7 +37,7 @@ public class BossTeleport : MonoBehaviour
 
         BombHitCount++;
 
-        if (BombHitCount >= Teleport[BossState].BombHitRequire)
+        if (BossState >= Teleport.Length - 1 || BombHitCount >= Teleport[BossState].BombHitRequire)
         {
             if (BossState < Teleport.Length - 1)
             {
@@ -66,6 +68,8 @@ public class BossTeleport : MonoBehaviour
                 {
                     EAP.willDestroy = true;
                 }
+
+                ClearEvent.Invoke();
             }
 
         }
