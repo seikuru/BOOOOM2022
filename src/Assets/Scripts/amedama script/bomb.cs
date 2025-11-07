27ced9551a5d6634477db28d3c7e9986933df182
ichiharayuuma <youma.20031209@gmail.com>
2025-11-07 18:59:45 +0900

    爆弾を投げる挙動を変更

--- src/Assets/Scripts/amedama script/bomb.cs
+++ src/Assets/Scripts/amedama script/bomb.cs
@@ -76,10 +76,12 @@ public class bomb : MonoBehaviour
 
         // 投げる力（プレイヤーの移動速度を加味する）
         // Bombthrow + プレイヤーの速度の大きさ × percentage
-        Vector3 _force = direction * (Bombthrow + PlayerRigidbody.velocity.magnitude) * percentage;
+        // Vector3 _force = (Bombthrow + PlayerRigidbody.velocity.magnitude) * percentage * direction;
+        Vector3 _force = Bombthrow * direction.normalized;
         // Vector3 _force = direction * ThrowPower + this.gameObject.GetComponent<Rigidbody>().linearVelocity * 0.4f;
 
-        Vector3 _Inertia = PlayerRigidbody.velocity * (1f - percentage);
+        // Vector3 _Inertia = PlayerRigidbody.velocity * (1f - percentage);
+        Vector3 _Inertia = PlayerRigidbody.velocity;
 
         // 投擲力を加える（Impulseは質量を考慮した一時的な力）
         Bomb_rb.AddForce(_force, ForceMode.Impulse);
