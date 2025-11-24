7c33af0f454545741ae82c1ad849d9bfb839c880
AmedamaSpica <spica96amedama@gmail.com>
2025-11-24 18:00:20 +0900

    オブジェクトを破壊したらBGMを追加

--- src/Assets/Scripts/amedama script/CollectObject.cs
+++ src/Assets/Scripts/amedama script/CollectObject.cs
@@ -13,7 +13,7 @@ public class CollectObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -43,6 +43,7 @@ public class CollectObject : MonoBehaviour
                 Debug.Log(CollectPoint);
                 once = false;
                 Destroy(this.gameObject);
+                
             }
         }
 
