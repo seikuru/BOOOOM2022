04dc36d4191778bf33f3602fd1b0fa83b7ed06f5
AmedamaSpica <spica96amedama@gmail.com>
2025-12-08 18:27:04 +0900

    BGMのfadeInを作成

--- src/Assets/Scripts/amedama script/BGMControll.cs
+++ src/Assets/Scripts/amedama script/BGMControll.cs
@@ -8,21 +8,31 @@ using UnityEngine;
     Bass,Drums,Chord,Melody
 }
 
+enum MusicEntry
+{
+    fadeIn, Measure
+}
+
+[Serializable]
+class AudioSourceClass
+{
+    public AudioSource AudioSource;
+    public MusicEntry entryType;
+}
+
 [Serializable]
 class CollectObjectClass
 {
     
-
     public MusicType COCMusicType;
     public GameObject[] CollectObject;
-   
 }
 
 [Serializable]
 class CollectMusicClass
 {
     public MusicType CMCMusicType;
-    public AudioSource[] AudioSources;
+    public AudioSourceClass[] ASC;
 }
 
 public class BGMControll : MonoBehaviour
@@ -36,65 +46,91 @@ public class BGMControll : MonoBehaviour
     [SerializeField]float Count = 0;
     [SerializeField]int BPM = 150;
     float OneMeasure;
+    Coroutine[] AudioCoroutine;
+    [SerializeField] float FadeInTime = 3.0f;
+    [SerializeField] float FirstBigTime = 3.0f;
+    float FadeInTimeOne;
+    int CoroutineCount = 0;
     // Start is called before the first frame update
     void Start()
     {
         OneMeasure = 240 / BPM;
 
+        FadeInTimeOne = FadeInTime * Time.fixedDeltaTime;
+        
         beforCollectPoint = CollectObject.CollectPoint;
+        int i = 0;
         foreach (var a1 in CMC)
         {
-            foreach (var b2 in a1.AudioSources)
+            foreach (var b2 in a1.ASC)
             {
-                if (b2 != null)
+                i++;
+                if (b2.AudioSource != null)
                 {
-                    b2.enabled = true;
-                    b2.mute = true;
+                    b2.AudioSource.enabled = true;
+                    b2.AudioSource.volume = 0;
                 }
             }
         }
+        AudioCoroutine = new Coroutine[i];
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+
         Count += Time.deltaTime;
 
-        Debug.Log(Count);
+
+        ObjectNumber = 0;
+        foreach (var b1 in COC)
+        {
+            foreach (var b2 in b1.CollectObject)
+            {
+
+                if (b2 == null)
+                {
+                    int i = 0;
+                    foreach (var a1 in CMC)
+                    {
+
+                        if (a1.CMCMusicType == b1.COCMusicType)
+                        {
+                            break;
+                        }
+                        i++;
+                    }
+
+                    if (CMC[i].ASC.Length > ObjectNumber
+                        && CMC[i].ASC[ObjectNumber].AudioSource != null)
+                    {
+                        if (CMC[i].ASC[ObjectNumber].entryType == MusicEntry.Measure 
+                            && CMC[i].ASC[ObjectNumber].AudioSource.volume == 0.0f)
+                        {
+                            if (Count >= OneMeasure)
+                            {
+                                AudioCoroutine[CoroutineCount] = StartCoroutine(firstBigAudio(CMC[i].ASC[ObjectNumber].AudioSource));
+                                CoroutineCount++;
+                            }
+                        }
+                        else if (CMC[i].ASC[ObjectNumber].entryType == MusicEntry.fadeIn 
+                            && CMC[i].ASC[ObjectNumber].AudioSource.volume == 0.0f)
+                        {
+
+                            AudioCoroutine[CoroutineCount] = StartCoroutine(FadeInAudio(CMC[i].ASC[ObjectNumber].AudioSource));
+                            CoroutineCount++;
+                        }
+                    }
+
+
+                    ObjectNumber++;
+                }
+
+            }
+            ObjectNumber = 0;
+        }
 
         if (Count >= OneMeasure)
         {
-            Debug.Log("o+wow");
-            ObjectNumber = 0;
-            foreach (var b1 in COC)
-            {
-                foreach (var b2 in b1.CollectObject)
-                {
-                    if (b2 == null)
-                    {
-                        int i = 0;
-                        foreach (var a1 in CMC)
-                        {
-
-                            if (a1.CMCMusicType == b1.COCMusicType)
-                            {
-                                break;
-                            }
-                            i++;
-                        }
-
-                        if (CMC[i].AudioSources.Length > ObjectNumber
-                            && CMC[i].AudioSources[ObjectNumber] != null)
-                        {
-                            CMC[i].AudioSources[ObjectNumber].mute = false;
-                        }
-                        ObjectNumber++;
-                    }
-
-                }
-                ObjectNumber = 0;
-            }
             Count = 0;
         }
     }
@@ -102,5 +138,29 @@ public class BGMControll : MonoBehaviour
     void AudioChange()
     {
         //AS[CollectObject.CollectPoint].mute = false;
+    }
+
+    IEnumerator FadeInAudio(AudioSource audio)
+    {
+        while (audio.volume < 0.9f)
+        {
+            audio.volume += FadeInTimeOne;
+            yield return null;
+        }
+
+        StopCoroutine(AudioCoroutine[CoroutineCount]);
+    }
+
+    IEnumerator firstBigAudio(AudioSource audio)
+    {
+
+        audio.volume = 1.0f;
+
+        yield return new WaitForSeconds(FirstBigTime);
+
+        audio.volume = 0.9f;
+
+        StopCoroutine(AudioCoroutine[CoroutineCount]);
+
     }
 }
