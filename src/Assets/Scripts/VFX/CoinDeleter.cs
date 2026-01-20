c2cb984c64b799e5bb46a2919eee6c39b67080ea
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2026-01-20 10:46:04 +0900

    チュートリアルの張替え

--- src/Assets/Scripts/VFX/CoinDeleter.cs
+++ src/Assets/Scripts/VFX/CoinDeleter.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class CoinDeleter : MonoBehaviour
 {
     [SerializeField] private VisualEffect coinEffect;
-    [SerializeField] private float delayDeleteTime = 2;
+    [SerializeField] private float delayDeleteTime = 3f;
     [SerializeField] private Collider coinCollider;
     [SerializeField] private Rigidbody rigidBody;
+    [SerializeField] private Transform coinTransform;
+    [SerializeField] private float ScaleMinusTime = 1f;
     bool isTake = false;
 
     public void TakeCoin()
@@ -24,9 +27,26 @@ public class CoinDeleter : MonoBehaviour
 
         coinEffect.SendEvent("OnHit");
 
+        StartCoroutine(ScaleMinus());
+
         Destroy(gameObject, delayDeleteTime);
     }
+    
+    IEnumerator ScaleMinus()
+    {
+        Vector3 local = coinTransform.localScale;
+        float count = 0f;
+        while (count < ScaleMinusTime)
+        {
+            count += Time.deltaTime;
 
+            float clamp = Mathf.Clamp01(count / ScaleMinusTime);
+
+            coinTransform.localScale = local * clamp;
+
+            yield return null;
+        }
+    }
     /*
     private void OnTriggerEnter(Collider other)
     {
