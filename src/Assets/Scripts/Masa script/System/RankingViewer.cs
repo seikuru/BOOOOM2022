d5e613b462d016da94168bc7544babb418cf6528
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2026-02-11 01:27:11 +0900

    ランキングのすくりぷちょ仮組

---
+++ src/Assets/Scripts/Masa script/System/RankingViewer.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class RankingViewer : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName = "ichi_yama";
+
+    [SerializeField]
+    bool GetCurrentMode = false;
+
+    [SerializeField]
+    TextMeshProUGUI[] scoreTexts;
+
+    static readonly int RankingValue = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreTexts == null || scoreTexts.Length != RankingValue)
+            return;
+
+        int[] score = GetCurrentMode ?
+            ScoreRanking.GetRankingDataBeforeMode():
+            ScoreRanking.GetRankingData(sceneName);
+
+        if (score == null || score.Length != RankingValue)
+            return;
+
+        for (int i = 0; i < RankingValue; i++)
+            scoreTexts[i].SetText(score[i].ToString());
+    }
+}
