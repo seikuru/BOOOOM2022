42c338b34e0be6b65404c065255fd2ab979d75ec
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2026-02-11 01:38:22 +0900

    リセットその2

---
+++ src/Assets/Scripts/Masa script/Input/ResetCommand.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetCommand : MonoBehaviour
+{
+    [SerializeField]
+    ScoreRanking scoreRanking;
+
+    private KeyCode[] command = new KeyCode[]
+     {
+        KeyCode.UpArrow,
+        KeyCode.UpArrow,
+        KeyCode.DownArrow,
+        KeyCode.DownArrow,
+        KeyCode.LeftArrow,
+        KeyCode.RightArrow,
+        KeyCode.LeftArrow,
+        KeyCode.RightArrow,
+        KeyCode.B,
+        KeyCode.A
+     };
+
+    private int index = 0;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(command[index]))
+            {
+                index++;
+
+                if (index >= command.Length)
+                {
+                    Debug.Log("コマンド成功！");
+                    index = 0;
+                    scoreRanking.ResetData();
+                }
+            }
+            else
+            {
+                // 間違えたらリセット
+                index = 0;
+            }
+        }
+    }
+}
